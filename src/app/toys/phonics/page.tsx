f8ac704f2f8fb08ee4dad666fa9e280011aad549
C Stavridis <castavridis@gmail.com>
2025-10-02 00:36:52 -0500

    Add feedback for character recognition

--- src/app/toys/phonics/page.tsx
+++ src/app/toys/phonics/page.tsx
@@ -7,6 +7,9 @@
 import Tesseract, { createWorker } from 'tesseract.js'
 import { GestureRecognizer, FilesetResolver, DrawingUtils, GestureRecognizerResult } from '@mediapipe/tasks-vision'
 import { useCallback, useEffect, useRef, useState } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
 
 const VIDEO_HEIGHT = "720px"
 const VIDEO_WIDTH = "960px"
@@ -15,10 +18,33 @@ export default function GestureRecognizerTest () {
   const [isLoading, setIsLoading] = useState(true)
   const [gestureRecognizer, setGestureRecognizer] = useState<GestureRecognizer>()
   const [lastVideoTime, setLastVideoTime] = useState(-1)
+  const [recognizedText, setRecognizedText] = useState(``)
   
   const videoRef = useRef(null)
   const canvasRef = useRef(null)
   const workerRef = useRef<Tesseract.Worker>(null)
+  const recognizedTextRef = useRef(null)
+
+  useGSAP(() => {
+    if (recognizedTextRef.current) {
+      gsap.to(recognizedTextRef.current, {
+        // x: 100 * Math.random(),
+        y: -100,
+        opacity: 1,
+        scale: 2.5,
+        duration: 0.5,
+        ease: 'power2.inOut',
+        yoyo: true,
+        repeat: 1,
+        yoyoEase: 'power2.inOut',
+        onComplete: () => {
+          gsap.to(recognizedTextRef.current, {
+            opacity: 0,
+          })
+        }
+      })
+    }
+  }, {dependencies: [recognizedText]})
 
   const initTesseract = useCallback(async () => {
     const worker = await createWorker('eng')
@@ -76,7 +102,7 @@ export default function GestureRecognizerTest () {
     const capture = canvasRef.current.toDataURL()
     const ret = await workerRef.current.recognize(capture)
     if (ret.data.text) {
-      console.log(ret.data.text)
+      setRecognizedText(`${ret.data.text}`)
     }
   }, [canvasRef, workerRef])
 
@@ -93,8 +119,11 @@ export default function GestureRecognizerTest () {
       for (const gestures of results.gestures) {
         for (const gesture of gestures) {
           if (gesture.categoryName !== "Pointing_Up") {
+            canvasCtx.scale(-1,1)
+            canvasCtx.save()
             recognizeText()
             canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
+            canvasCtx.restore()
           }
           else {
             if (results.landmarks) {
@@ -112,7 +141,7 @@ export default function GestureRecognizerTest () {
                 //   lineWidth: 1,
                 // })
                 drawingUtils.drawLandmarks([landmarks[8]], {
-                  color: "#000000",
+                  color: "#FF0000",
                   lineWidth: 5,
                 })
               }
@@ -163,18 +192,19 @@ export default function GestureRecognizerTest () {
     isLoading 
       ? <div>
           Loading Phonics Toy...
-        </div> 
+        </div>
       : <div>
           <h1>Phonics Toy</h1>
           <div className="flex items-center">
             <h2 className="mb-0">1. Enable Webcam</h2>
             <button className="rounded-md block p-3 ml-4" onClick={handleButtonClick}>Enable Webcam</button>
           </div>
-          <h2>2. Attempt to draw letters using 👆 your pointer finger</h2>
-          <h2>3. When you're ready to check your letter, close your fist ✊</h2>
-          <div style={{
-            position: "relative"
-          }}>
+          <h2>Goals</h2>
+          <ul>
+            <li>Attempt to draw letters using 👆 your pointer finger</li>
+            <li>When you're ready to check your letter, close your fist ✊</li>
+          </ul>
+          <div className="relative">
             <video className="scale-x-[-1]" ref={videoRef} autoPlay playsInline style={{ opacity: 0.25 }} />
             <canvas className="scale-x-[-1]" ref={canvasRef} width="1280" height="720" style={{
               position: "absolute",
@@ -183,7 +213,9 @@ export default function GestureRecognizerTest () {
               pointerEvents: "none",
               zIndex: 1,
             }}></canvas>
-
+            <div className="absolute top-[50%] left-[50%] z-10 opacity-0 translate-x-[-50%] translate-y-[-50%] text-5xl" ref={recognizedTextRef}>
+              {recognizedText}
+            </div>
           </div>
         </div>
   )
