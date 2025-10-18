1e4f449525256a197fa35e1749e1bb22c1743af9
C Stavridis <castavridis@gmail.com>
2025-10-18 15:53:23 -0500

    Finish port to React; fix end shape logic

--- src/app/toys/calder/bezierTool.tsx
+++ src/app/toys/calder/bezierTool.tsx
@@ -1,3 +1,6 @@
+// @ts-nocheck: p5.js v2.0.5 types are not fully implement or correct
+// opportunity to contribute!
+
 "use client"
 
 import { Grid } from 'pretty-grid'
@@ -9,190 +12,182 @@ const MODE = {
   OCPOINT: 1, // Origin's control point
   DESTINATION: 2, // Destination
   DCPOINT: 3, // Destination's control point
-};
-const MODE_KEYS = Object.keys(MODE);
-const MODE_COLORS = ['#F0F', '#FF0', '#0FF', '#0F0'];
-const SKETCH_WIDTH = 500;
-const SKETCH_HEIGHT = 500;
-const SKETCH_GREY = 220;
+}
+const MODE_KEYS = Object.keys(MODE)
+const MODE_COLORS = ['#F0F', '#FF0', '#0FF', '#0F0']
+const SKETCH_WIDTH = 500
+const SKETCH_HEIGHT = 500
+const SKETCH_GREY = 220
 
 const s = (sketch: p5) => {
-  const curves = [];
+  const curves = []
   let currCurve = 0,
       currMode = MODE.ORIGIN,
       font,
       grid,
       pointer,
-      pointerDown = false;
+      pointerDown = false
 
   function setMode () {
-  if (currMode === MODE.ORIGIN && curves.length !== 0) {
-    updateCurve();
-    currCurve++;
+    updateCurve()
+    // Currently DCPOINT is the final point necessary to complete the bezier
+    if (currMode === MODE.DCPOINT) {
+      currCurve++
+    }
+    currMode = (currMode + 1) % MODE_KEYS.length
   }
-    updateCurve();
-    currMode = (currMode + 1) % MODE_KEYS.length;
-  }
-
   function updateCurve() {
-    const c = {
-      d: 5,
-    };
-    c[`x${currMode}`] = sketch.mouseX;
-    c[`y${currMode}`] = sketch.mouseY;
+    const c = {}
+    c[`x${currMode}`] = sketch.mouseX
+    c[`y${currMode}`] = sketch.mouseY
     curves[currCurve] = {
       ...curves[currCurve],
       ...c,
-    };
-  }
-
-  function drawPointer() {
-    const pointerColor = sketch.color(MODE_COLORS[currMode]);
-    if (pointerDown) {
-      pointerColor.setAlpha(255);
-      sketch.fill(pointerColor);
-      pointer = sketch.circle(sketch.mouseX, sketch.mouseY, 10);
-    } else {
-      pointerColor.setAlpha(125);
-      sketch.fill(pointerColor);
-      pointer = sketch.circle(sketch.mouseX, sketch.mouseY, 7.5);
     }
-    sketch.fill(0);
-
-    const pX = sketch.mouseX - 50;
-
-    const pY = SKETCH_HEIGHT - sketch.mouseY - 50;
-
-    sketch.text(`(${pX.toFixed(0)},${pY.toFixed(0)})`, sketch.mouseX - 2.5, sketch.mouseY - 10, 100, 100);
   }
-
-  function drawDotGrid () {
-    sketch.fill(200);
-    
-    // Draw dots for all the points on the grid
-    grid.every(({ x, y }) => sketch.circle(x, y, 2));
-  }
-
   function hasProperties(data, arr) {
     return arr.every((r) => data.hasOwnProperty(r))
   }
-
   // in p5.js, origin = top-left corner
-function drawCurves () {
-  for (let i = 0; i < curves.length; i++) {
-    const c = curves[i];
-    if (c) {
-      
-      // Draw bezier curve
-      sketch.stroke(0);
-      sketch.strokeWeight(1.5);
-      const fillC = sketch.color('antiquewhite');
-      fillC.setAlpha(150);
-      sketch.fill(fillC);
-      if (hasProperties(c, ['x0','y0','x1','y1','x2','y2','x3','y3'])) {
-        sketch.bezier(c.x0, c.y0, c.x1, c.y1, c.x3, c.y3, c.x2, c.y2); 
+  function drawCurves () {
+    for (let i = 0; i < curves.length; i++) {
+      const c = curves[i]
+      if (c) {
+        
+        // Draw bezier curve
+        sketch.stroke(0)
+        sketch.strokeWeight(1.5)
+        const fillC = sketch.color('antiquewhite')
+        fillC.setAlpha(150)
+        sketch.fill(fillC)
+        if (hasProperties(c, ['x0','y0','x1','y1','x2','y2','x3','y3'])) {
+          sketch.bezier(c.x0, c.y0, c.x1, c.y1, c.x3, c.y3, c.x2, c.y2) 
+        }
+        
+        // Draw control point lines
+        sketch.noFill()
+        sketch.stroke(210)
+        sketch.strokeWeight(1)
+        
+        // Draw line from origin to control point
+        if (hasProperties(c, ['x0','y0','x1','y1'])) {
+          sketch.line(c.x1, c.y1, c.x0, c.y0) 
+        }
+        
+        // Draw line from destination to control point
+        if (hasProperties(c, ['x2','y2','x3','y3'])) {
+          sketch.line(c.x2, c.y2, c.x3, c.y3)
+        }
+        
+        // Draw points of bezier
+        sketch.noStroke()
+        
+        const diameter = 5
+        if (hasProperties(c, ['x0','y0'])) {
+          sketch.fill(MODE_COLORS[MODE.ORIGIN])
+          sketch.circle(c.x0, c.y0, diameter)
+        }
+        if (hasProperties(c, ['x1','y1'])) {
+          sketch.fill(MODE_COLORS[MODE.OCPOINT])
+          sketch.circle(c.x1, c.y1, diameter)
+        }
+        if (hasProperties(c, ['x2','y2'])) {
+          sketch.fill(MODE_COLORS[MODE.DESTINATION])
+          sketch.circle(c.x2, c.y2, diameter)
+        }
+        if (hasProperties(c, ['x3','y3'])) {
+          sketch.fill(MODE_COLORS[MODE.DCPOINT])
+          sketch.circle(c.x3, c.y3, diameter)  
+        }
+        sketch.noFill()
       }
-      
-      // Draw control point lines
-      sketch.noFill();
-      sketch.stroke(210);
-      sketch.strokeWeight(1);
-      
-      // Draw line from origin to control point
-      if (hasProperties(c, ['x0','y0','x1','y1'])) {
-        sketch.line(c.x1, c.y1, c.x0, c.y0); 
-      }
-      
-      // Draw line from destination to control point
-      if (hasProperties(c, ['x2','y2','x3','y3'])) {
-        sketch.line(c.x2, c.y2, c.x3, c.y3);
-      }
-      
-      // Draw points of bezier
-      sketch.noStroke();
-      
-      if (hasProperties(c, ['x0','y0'])) {
-        sketch.fill(MODE_COLORS[MODE.ORIGIN]);
-        sketch.circle(c.x0, c.y0, c.d);
-      }
-      if (hasProperties(c, ['x1','y1'])) {
-        sketch.fill(MODE_COLORS[MODE.OCPOINT]);
-        sketch.circle(c.x1, c.y1, c.d);
-      }
-      if (hasProperties(c, ['x2','y2'])) {
-        sketch.fill(MODE_COLORS[MODE.DESTINATION]);
-        sketch.circle(c.x2, c.y2, c.d);
-      }
-      if (hasProperties(c, ['x3','y3'])) {
-        sketch.fill(MODE_COLORS[MODE.DCPOINT]);
-        sketch.circle(c.x3, c.y3, c.d);  
-      }
-      sketch.noFill();
     }
   }
-}
 
+  /** UI ELEMENTS GO HERE */
+  function drawPointer() {
+    const pointerColor = sketch.color(MODE_COLORS[currMode])
+    if (pointerDown) {
+      pointerColor.setAlpha(255)
+      sketch.fill(pointerColor)
+      pointer = sketch.circle(sketch.mouseX, sketch.mouseY, 10)
+    } else {
+      pointerColor.setAlpha(125)
+      sketch.fill(pointerColor)
+      pointer = sketch.circle(sketch.mouseX, sketch.mouseY, 7.5)
+    }
+    sketch.fill(0)
+
+    const pX = sketch.mouseX - 50
+
+    const pY = SKETCH_HEIGHT - sketch.mouseY - 50
+
+    sketch.text(`(${pX.toFixed(0)},${pY.toFixed(0)})`, sketch.mouseX - 2.5, sketch.mouseY - 10, 100, 100)
+  }
+  function drawDotGrid () {
+    sketch.fill(200)
+    
+    // Draw dots for all the points on the grid
+    grid.every(({ x, y }) => sketch.circle(x, y, 2))
+  }
   function drawAxes () {
-    sketch.stroke(180);
-    sketch.strokeWeight(1.5);
-    const margin = 50;
+    sketch.stroke(180)
+    sketch.strokeWeight(1.5)
+    const margin = 50
     // y-axis
-    sketch.line(margin, margin, margin, SKETCH_HEIGHT-margin);
+    sketch.line(margin, margin, margin, SKETCH_HEIGHT-margin)
     // x-axis
-    sketch.line(margin, SKETCH_HEIGHT - margin, SKETCH_WIDTH - margin, SKETCH_HEIGHT-margin);
-    sketch.noStroke();
+    sketch.line(margin, SKETCH_HEIGHT - margin, SKETCH_WIDTH - margin, SKETCH_HEIGHT-margin)
+    sketch.noStroke()
   }
-
   function drawModeLabel () {
-    const label = `Mode: ${MODE_KEYS[currMode]}`;
-    const margin = 5;
-    const bounds = font.textBounds(label, 5, 15);
-    const c = sketch.color(MODE_COLORS[currMode]);
-    c.setAlpha(50);
-    sketch.fill(c);
-    sketch.rect(bounds.x - margin/2,bounds.y - margin/2,bounds.w +margin *2,bounds.h + margin*2);
-    sketch.fill(0);
-    sketch.text(label,5+margin/2,15+margin/2);
+    const label = `Mode: ${MODE_KEYS[currMode]}`
+    const margin = 5
+    const bounds = font.textBounds(label, 5, 15)
+    const c = sketch.color(MODE_COLORS[currMode])
+    c.setAlpha(50)
+    sketch.fill(c)
+    sketch.rect(bounds.x - margin/2,bounds.y - margin/2,bounds.w +margin *2,bounds.h + margin*2)
+    sketch.fill(0)
+    sketch.text(label,5+margin/2,15+margin/2)
   }
-
-
-  function drawCurveCount () {
-    const label = `Curves: ${curves.length}`;
-    const margin = 5;
-    const bounds = font.textBounds(label, 5, SKETCH_HEIGHT-15);
-    sketch.fill(220);
-    sketch.rect(bounds.x - margin/2,bounds.y - margin/2,bounds.w +margin *2,bounds.h + margin*2);
-    sketch.fill(0);
-    sketch.text(label,5+margin/2,SKETCH_HEIGHT-15+margin/2);
+  function drawCurveCountLabel () {
+    const label = `Curves: ${curves.length}`
+    const margin = 5
+    const bounds = font.textBounds(label, 5, SKETCH_HEIGHT-15)
+    sketch.fill(220)
+    sketch.rect(bounds.x - margin/2,bounds.y - margin/2,bounds.w +margin *2,bounds.h + margin*2)
+    sketch.fill(0)
+    sketch.text(label,5+margin/2,SKETCH_HEIGHT-15+margin/2)
   }
-
   function drawUI () {
-    drawDotGrid();
-    drawAxes();
-    drawModeLabel();
-    drawCurveCount();
-    drawPointer();
+    drawDotGrid()
+    drawAxes()
+    drawModeLabel()
+    drawCurveCountLabel()
+    drawPointer()
   }
 
-  sketch.mousePressed = () => {
-    pointerDown = true;
-    setMode();
-  }
   sketch.mouseReleased = () => {
-    pointerDown = false;
+    pointerDown = false
+  }
+  sketch.mousePressed = () => {
+    pointerDown = true
+    setMode()
   }
   sketch.keyPressed = () => {
     if (sketch.keyCode === 27) { // ESC pressed
-      currMode = MODE.ORIGIN;
+      currMode = MODE.ORIGIN
     }
   }
   sketch.draw = () => {
-    sketch.background(255);
-    sketch.noStroke();
-    drawUI();
-    drawCurves();
+    sketch.background(255)
+    sketch.noStroke()
+    drawUI()
+    drawCurves()
   }
   sketch.setup = async () => {
+    // Try to preload items
     try {
       // p5.js doesn't know what the client's default font is
       // So a font must be loaded before we can get textBounds dynamically
@@ -200,18 +195,15 @@ function drawCurves () {
         '/assets/fonts/Karla-Medium.ttf',
         (data) => { 
           font = sketch.textFont(data, 12)
-          console.log(font)
         },
         (err) => { 
           console.warn('Could not load preferred font. Attempting to load fallback.', err)
           font = sketch.textFont('Arial', 12)
-          console.log(font)
         }
       )
     } catch(err) {
       console.warn('Could not load preferred font. Attempting to load fallback.', err)
       font = sketch.textFont('Arial', 12)
-      console.log(font)
     }
     // No need to call sketch.createCanvas in instance mode
     sketch.resizeCanvas(SKETCH_WIDTH,SKETCH_HEIGHT,true)
@@ -228,14 +220,9 @@ export default function BezierDrawingTool (): React.ReactElement {
     if (containerRef.current && !sketchRef.current) {
       sketchRef.current = new p5(s, containerRef.current)
     }
-    return () => {
-      // FIXME this cleanup creates multiple components
-      // if (sketchRef.current) {
-      //   // Stop p5 draw loop and remove HTML elements created by sketch
-      //   sketchRef.current.remove()
-      //   sketchRef.current = null
-      // }
-    }
+
+    // Clean up sketch
+    return () => sketchRef.current?.remove()
   }, [])
 
   return (
