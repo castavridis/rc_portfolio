2c42840fecafb0a00d6eef46fd8327f4fa6915ce
C Stavridis <castavridis@gmail.com>
2025-10-18 21:36:21 -0500

    Ignore types during build

--- next.config.js
+++ next.config.js
@@ -6,5 +6,12 @@ export default {
   },
   turbopack: {
     root: "/Users/cstavridis/Git/rc_portfolio"
-  }
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
