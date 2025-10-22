f9f0296a7167d4d1efb7566b19fb90a79faff4e0
C Stavridis <castavridis@gmail.com>
2025-10-21 23:20:49 -0500

    Add Rapier to Threejs experiment

---
+++ src/app/toys/calder/bezierPhysicsScene.tsx
@@ -0,0 +1,15 @@
+import r2D from '@dimforge/rapier2d'
+import p5 from 'p5'
+import { useEffect } from 'react'
+
+export default function BezierPhysicsScene ({
+  shapes
+}): React.ReactNode {
+  useEffect(() => {}, [shapes])
+  
+  return (
+    <div>
+
+    </div>
+  )
+}
