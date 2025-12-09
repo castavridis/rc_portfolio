55dfc944fcf8aad3ed92d71428ed74c3fe1d7fe3
C Stavridis <castavridis@gmail.com>
2025-12-09 12:45:58 -0500

    remove eslint from config

--- next.config.ts
+++ next.config.ts
@@ -21,11 +21,11 @@ const nextConfig: NextConfig = {
 
 export default {
   ...nextConfig,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   // Warning: This allows production builds to successfully complete even if
+  //   // your project has ESLint errors.
+  //   ignoreDuringBuilds: true,
+  // },
   turbopack: {
     root: "/Users/cstavridis/Git/rc_portfolio"
   },
