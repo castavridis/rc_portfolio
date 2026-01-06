61bd9250716e47628a481773b7f7165fad4e37f3
C Stavridis <castavridis@gmail.com>
2026-01-06 16:39:20 -0600

    Add stub for Notate toy

---
+++ src/app/toys/notate/page.tsx
@@ -0,0 +1,12 @@
+export default function NotatePage() {
+  const system_prompt = ``
+  const prompt = ""
+  const response = ""
+  return (
+    <div>
+      {system_prompt}
+      {prompt}
+      {response}
+    </div>
+  )
+}
